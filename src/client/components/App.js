c00d8884de246aaf8b686a28b359610c7c492b72
Steve <bevets87@gmail.com>
2017-05-06 23:54:59 -0400

    fixed client socket connection

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import _ from 'lodash'
 
 import io from 'socket.io-client'
-const socket = io.connect('http://localhost:3000')
+const socket = io.connect('https://fcc-stock-market-application.herokuapp.com/')
 
 import StockChart from './StockChart'
 
